554a341e18529e2dda12ec173ceb7ffc07a992aa
DJ Bauch <dj.bauch@gmail.com>
2023-01-23 22:24:36 -0600

    Completed as described in tutorial

--- server/src/schema.js
+++ server/src/schema.js
@@ -52,6 +52,23 @@ const typeDefs = gql`
     "The module's video url, for video-based modules"
     videoUrl: String
   }
+
+
+   type Mutation {
+      "Increment the number of views of a given track when the track card is clicked"
+      incrementTrackViews(id: ID!): IncrementTrackViewsResponse!
+   }
+
+   type IncrementTrackViewsResponse {
+    "Similar to HTTP status code, represents the status of the mutation"
+    code: Int!
+    "Indicates whether the mutation was successful"
+    success: Boolean!
+    "Human-readable message for the UI"
+    message: String!
+    "Newly updated track after a successful mutation"
+    track: Track
+   }
 `;
 
 module.exports = typeDefs;
